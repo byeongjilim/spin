e5ec89025bbb765d329ba1304545553bdbef72aa
Jay Lim <byeongjilim@gmail.com>
2021-07-10 17:59:46 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# SPIN
+Severless Private Intersyncable Notes
+
+I'm creating a notes app that doesn't use any servers, and only saves data between devices that you own.
+
+### To-dos
+[ ] Create a basic notes app.
+[ ] Allow saving on the device.
+[ ] To be continued...
+
---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      note: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ note: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    localStorage.setItem("note", this.state.note);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <textarea value={this.state.note} onChange={this.handleChange} />
+          <input type="submit" value="Save" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
